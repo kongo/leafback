f784fd646759387dd8bc24e4d374c468165a4de6
kongo <d.parshenko@gmail.com>
2012-07-12 12:00:57 +0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Leafback::Application.config.secret_token = 'aa768f50a6b5b26cfe55ecfba4fcaa08ca26c0159ab3703a8ff15e3e305602674dbe5f6fff3d1b75baabbfada6ce32ae043e1e55b4233785b468cec5914fab5f'
