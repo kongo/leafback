c13f6e9ba2637f94fafc1224b7b4506fdf09d1e6
kongo <d.parshenko@gmail.com>
2012-07-13 00:46:46 +0300

    no assets in server log

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,3 +35,20 @@ Leafback::Application.configure do
   # Expands the lines which load the assets
   config.assets.debug = true
 end
+
+class DisableAssetsLogger
+  def initialize(app)
+    @app = app
+    Rails.application.assets.logger = Logger.new('/dev/null')
+  end
+
+  def call(env)
+    previous_level = Rails.logger.level
+    Rails.logger.level = Logger::ERROR if env['PATH_INFO'].index("/assets/") == 0
+    @app.call(env)
+  ensure
+    Rails.logger.level = previous_level
+  end
+end
+
+Rails.application.config.middleware.insert_before Rails::Rack::Logger, DisableAssetsLogger
