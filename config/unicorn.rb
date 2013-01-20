17d07b4b0a98bfdd7fbd7687a108154152d350d1
dparshenko <dparsh@softserveinc.com>
2013-01-21 01:49:04 +0200

    Unicorn

---
+++ config/unicorn.rb
@@ -0,0 +1,69 @@
+# Set environment to development unless something else is specified
+env = ENV["RAILS_ENV"] || "development"
+ 
+# See http://unicorn.bogomips.org/Unicorn/Configurator.html for complete documentation.
+worker_processes 2 # amount of unicorn workers to spin up
+ 
+listen "/tmp/homepage.socket"
+ 
+preload_app true
+ 
+timeout 30         # restarts workers that hang for 30 seconds
+ 
+pid "/tmp/unicorn.homepage.pid"
+ 
+if env == "production"
+  # Help ensure your application will always spawn in the symlinked
+  # "current" directory that Capistrano sets up.
+  working_directory "/u/apps/homepage/current"
+ 
+  # feel free to point this anywhere accessible on the filesystem
+  user 'kongo', 'kongo' # 'user', 'group'
+  shared_path = "/u/apps/homepage/shared"
+ 
+  stderr_path "#{shared_path}/log/unicorn.stderr.log"
+  stdout_path "#{shared_path}/log/unicorn.stdout.log"
+end
+ 
+before_fork do |server, worker|
+  # the following is highly recomended for Rails + "preload_app true"
+  # as there's no need for the master process to hold a connection
+  if defined?(ActiveRecord::Base)
+    ActiveRecord::Base.connection.disconnect!
+  end
+ 
+ 
+  # When sent a USR2, Unicorn will suffix its pidfile with .oldbin and
+  # immediately start loading up a new version of itself (loaded with a new
+  # version of our app). When this new Unicorn is completely loaded
+  # it will begin spawning workers. The first worker spawned will check to
+  # see if an .oldbin pidfile exists. If so, this means we've just booted up
+  # a new Unicorn and need to tell the old one that it can now die. To do so
+  # we send it a QUIT.
+  #
+  # This enables 0 downtime deploys.
+  old_pid = "/tmp/unicorn.homepage.pid.oldbin"
+  if File.exists?(old_pid) && server.pid != old_pid
+    begin
+      Process.kill("QUIT", File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
+      # someone else did our job for us
+    end
+  end
+end
+ 
+after_fork do |server, worker|
+ 
+  # Unicorn master loads the app then forks off workers - because of the way
+  # Unix forking works, we need to make sure we aren't using any of the parent's
+  # sockets, e.g. db connection (since "preload_app true")
+  if defined?(ActiveRecord::Base)
+    ActiveRecord::Base.establish_connection
+  end
+ 
+  # if preload_app is true, then you may also want to check and
+  # restart any other shared sockets/descriptors such as Memcached,
+  # and Redis.  TokyoCabinet file handles are safe to reuse
+  # between any number of forked children (assuming your kernel
+  # correctly implements pread()/pwrite() system calls)
+end
